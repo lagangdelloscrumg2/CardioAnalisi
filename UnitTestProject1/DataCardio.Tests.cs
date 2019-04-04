6612d04247170668dfa8ae6e58e00852b161a106
studenti <studenti@rosselli.loc>
2019-04-04 09:11:58 +0200

    Add project files.

---
+++ UnitTestProject1/DataCardio.Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CardioLibrary;
+
+namespace DataCardio.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
