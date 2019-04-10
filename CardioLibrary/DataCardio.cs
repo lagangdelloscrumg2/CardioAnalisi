93672ef3cff5e4bb8302d05e6a4a478971ee6c6e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-10 09:18:38 +0200

    miglioramenti eaggiunta di test per calcolare frequenze efficaci

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -8,9 +8,28 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
-        public static int Metodo1(int età)
+        public static string Metodo1(int età)
         {
-            return 0;
+            string analisi = "";
+            if (età > 0 && età < 220)
+            {
+                int Freq_Max;
+                Freq_Max = 220 - età;
+
+                double Freq_Max_Eff;
+                double Freq_Min_Eff;
+
+                Freq_Max_Eff = Freq_Max * 0.9;
+                Freq_Min_Eff = Freq_Max * 0.7;
+
+                analisi = $"la frequenza massima efficace è {Freq_Max_Eff}, quella minima è {Freq_Min_Eff}";
+            }
+            else if (età <= 0)
+                analisi = "inserire un numero maggiore di zero";
+            else
+                analisi = "inserire un numero minore di 220";
+
+            return analisi;
         }
     }
 }
--- UnitTestProject1/DataCardio.Tests.cs
+++ UnitTestProject1/DataCardio.Tests.cs
@@ -11,12 +11,25 @@ namespace DataCardio.Tests
         public void TestMethod1()
         {
             int età;
-            int exp;
-            int obt;
+            string exp;
+            string obt;
 
             età = 21;
-            exp = 199;
+            exp = "la frequenza massima efficace è 179,1, quella minima è 139,3";
             obt =  CardioLibrary.DataCardio.Metodo1(età);
+            Assert.AreEqual(obt, exp);
+        }
+
+        [TestMethod]
+        public void TestMethod1_1()
+        {
+            int età;
+            string exp;
+            string obt;
+
+            età = -21;
+            exp = "inserire un numero maggiore di zero";
+            obt = CardioLibrary.DataCardio.Metodo1(età);
             Assert.AreEqual(obt, exp);
         }
     }
