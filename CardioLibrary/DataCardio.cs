a49adbf9e539156b2b089b38eb32b9d477065052
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-17 09:08:06 +0200

    abbiamo testato il terzo metodo

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -21,7 +21,7 @@ namespace CardioLibrary
 
                 Freq_Max_Eff = Freq_Max * 0.9;
                 Freq_Min_Eff = Freq_Max * 0.7;
-                
+
                 analisi = $"la frequenza massima efficace è {Convert.ToInt32(Freq_Max_Eff)}, quella minima è {Convert.ToInt32(Freq_Min_Eff)}";
             }
             else if (età <= 0)
@@ -43,17 +43,69 @@ namespace CardioLibrary
 
                     if (battiti < 60)
                         analisi = "brachicardia";
-                    else if (battiti >100)
+                    else if (battiti > 100)
                         analisi = "tachicardia";
                     else
                         analisi = "normale";
                 }
                 else
                     analisi = "inserire un valore ragionevole";
-            } 
+            }
             else
                 analisi = "inserire un valore maggiore di zero";
             return analisi;
         }
+        public static string Kcal(char sesso, double freq, double peso, double età, double tempo)
+        {
+            //si inizializza la varaibile del risultato
+            string analisi = "";
+
+            if (sesso == 'M' || sesso == 'F')
+            {
+                if (freq > 0 && freq < 300)
+                {
+                    if (peso > 0 && peso < 400)
+                    {
+                        if (età > 0 && età < 130)
+                        {
+                            if (tempo > 0 && tempo < 400)
+                            {
+                                if (sesso == 'F')
+                                {
+                                    età *= 0.074;
+                                    peso *= 0.126;
+                                    freq *= 0.4472;
+                                    tempo /= 4.184;
+                                    //si esegue il calcolo finale arrotndando alla terza cifra
+                                    analisi = (Math.Round((età - peso + freq - 20.4022) * tempo, 3)).ToString();
+                                }
+                                else
+                                {
+                                    età *= 0.2107;
+                                    peso *= 0.199;
+                                    freq *= 0.6309;
+                                    tempo /= 4.184;
+                                    //si esegue il calcolo finale arrotndando alla terza cifra
+                                    analisi = (Math.Round((età + peso + freq - 55.0969) * tempo, 3)).ToString();
+                                }
+                            }
+                            else
+                                analisi = "inserire un tempo accettabile";
+                        }
+                        else
+                            analisi = "inserire un'età accettabile";
+                    }
+                    else
+                        analisi = "inserire un peso accettabile";
+                }
+                else
+                    analisi = "inserire una frequanza accettabile";
+            }
+            else
+                analisi = "inserire solo 'M' o 'F'";
+
+            return analisi;
+        }
     }
 }
+   
